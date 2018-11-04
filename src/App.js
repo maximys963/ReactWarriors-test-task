8c1d0f1f5483fe6a698f62470dba62254486dbb0
maximys963 <maxrap7878@mail.ru>
2018-11-05 01:03:49 +0200

    final version

--- src/App.js
+++ src/App.js
@@ -40,21 +40,16 @@ class App extends Component {
                        })
                     }
                 })
-
-
-
         }
-
     };
 
     render() {
         return (
             <div className='container'>
                 <div className='row'>
-                    <div className='col-md-10'><MovieList movies={this.state.moviesData} chooseItem={this.onMovieItemClick} /></div>
+                    <div className='col-md-10'><MovieList movies={this.state.moviesData} chooseItem={this.onMovieItemClick}/></div>
                     <div className='col-md-2'>
                         <MovieListWillWatch featureMovies={this.state.moviesWillWatch} />
-                        {console.log(this.state.moviesWillWatch)}
                     </div>
 
                 </div>
--- src/components/movieListWillWatch.js
+++ src/components/movieListWillWatch.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
+import '.././styles/movieListWillWatch.css'
 
 
 class MovieListWillWatch extends Component {
     render() {
         return (
-            <div>
+            <div className='movie-will-container'>
+                <h4>{`Will watch: ${this.props.featureMovies.length} films`}</h4>
                 <ul className="list-group">
                     {this.props.featureMovies.map(
                         (elem, index) => {
                             return(
-                                <li key={index} className="list-group-item">{`${elem.title}: ${elem.rating}`}</li>
+                                <li key={index} className="list-group-item">{`${elem.title}  ${elem.rating}`}</li>
                             )
                         }
                     )}
